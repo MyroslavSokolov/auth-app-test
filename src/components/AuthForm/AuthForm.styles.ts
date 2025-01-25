051cee046fb7f3d066a48886a4e942928bc0ce29
Myroslav Sokolov <alexazimov333@gmail.com>
2025-01-25 17:33:52 +0100

    WIP: Add show/hide password icon

--- src/components/AuthForm/AuthForm.styles.ts
+++ src/components/AuthForm/AuthForm.styles.ts
@@ -5,27 +5,98 @@ export const FormContainer = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    padding: 20px;
+    width: 315px;
+`;
+
+export const SignUpTxt = styled.p`
+    font-size: 28px;
+    font-weight: 700;
+    line-height: 28px;
+    color: #4A4E71;
+    text-align: center;
 `;
 
 export const Input = styled.input`
-    width: 100%;
-    padding: 10px;
-    margin: 10px 0;
-    border: 1px solid #ccc;
-    border-radius: 4px;
+    //width: 100%;
+    //padding: 10px;
+    //margin: 10px 0;
+    //border: 1px solid #ccc;
+    //border-radius: 4px;
+
+    //width: 311px;
+    //height: 56px;
+    //top: 528px;
+    //left: 140px;
+    //gap: 0px;
+    //border-radius: 60px;
+    //border: 2px 0px 0px 0px;
+
+    width: 315px;
+    height: 48px;
+    padding: 10px 10px 10px 20px;
+    gap: 10px;
+    border-radius: 10px;
+    border: 1px solid #ccc; // TODO fix here
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 19.36px;
+
+    &:focus {
+        border-color: #007bff;
+    }
 `;
 
-export const Button = styled.button`
-    padding: 10px;
-    background-color: #007bff;
-    color: white;
-    border: none;
-    border-radius: 4px;
-    cursor: pointer;
+export const EmailInput = styled(Input)`
 
-    &:disabled {
-        background-color: #ccc;
-        cursor: not-allowed;
-    }
+`;
+
+export const PasswordContainer = styled.div`
+    position: relative;
+    margin-top: 20px;
+`;
+
+export const PasswordInput = styled(Input)`
+`;
+
+export const ShowHidePasswordButton = styled.div<{ iconColor: string }>`
+    cursor: pointer;
+    position: absolute;
+    top: 60%;
+    right: 15px;
+    transform: translateY(-60%);
+
+    // svg {
+        //   fill: ${({ iconColor }) => iconColor};
+    //   width: 20px;
+    //   height: 20px;
+    //   transition: fill 0.3s ease;
+    // }
+`;
+
+export const PasswordRulesContainer = styled.div`
+    text-align: left;
+    padding: 0 20px;
+    margin-top: 20px;
+    font-size: 13px;
+    font-weight: 400;
+    line-height: 18px;
+    color: #4A4E71;
+`;
+
+export const SignUpButton = styled.button`
+    width: 240px;
+    height: 48px;
+    padding: 15px 32px 15px 32px;
+    gap: 10px;
+    border-radius: 30px;
+    background: linear-gradient(110.46deg, #70C3FF 12.27%, #4B65FF 93.92%);
+    border: none;
+    cursor: pointer;
+    margin-top: 156px;
+
+    font-size: 16px;
+    font-weight: 700;
+    line-height: 14px;
+    text-align: center;
+    color: white;
 `;
--- src/components/AuthForm/AuthForm.tsx
+++ src/components/AuthForm/AuthForm.tsx
@@ -2,8 +2,17 @@ import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { validatePassword } from '../../hooks/useValidation';
 import { emailRegex } from '../../utils/regex';
-import { FormContainer, Input, Button } from './AuthForm.styles';
+import {
+  EmailInput,
+  FormContainer, PasswordContainer,
+  PasswordInput,
+  PasswordRulesContainer,
+  SignUpButton,
+  SignUpTxt, ShowHidePasswordButton,
+} from './AuthForm.styles';
 import SuccessScreen from '../SuccessScreen/SuccessScreen';
+import HidePasswordIcon from '../icons/HidePasswordIcon.tsx';
+import ShowPasswordIcon from '../icons/ShowPasswordIcon.tsx';
 
 interface FormData {
   email: string;
@@ -15,10 +24,20 @@ const AuthForm: React.FC = () => {
     register,
     handleSubmit,
     setError,
+    clearErrors,
     formState: { errors },
   } = useForm<FormData>();
 
   const [isSuccess, setIsSuccess] = useState(false);
+  const [passwordValidationErrors, setPasswordValidationErrors] = useState<string[]>([]);
+
+  const [showPassword, setShowPassword] = useState(false);
+  const [iconColor, setIconColor] = useState('#aaa'); // Default icon color
+
+  const togglePasswordVisibility = () => {
+    setShowPassword((prev) => !prev);
+    setIconColor(showPassword ? '#aaa' : '#007bff'); // Change color on toggle
+  };
 
   const onSubmit = (data: FormData) => {
     const passwordErrors = validatePassword(data.password);
@@ -35,26 +54,84 @@ const AuthForm: React.FC = () => {
     }
   };
 
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const email = event.target.value;
+    if (!emailRegex.test(email)) {
+      setError('email', { type: 'manual', message: 'Invalid email format' });
+    } else {
+      clearErrors('email');
+    }
+  };
+
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const password = event.target.value;
+    const errors = validatePassword(password);
+    setPasswordValidationErrors(errors);
+
+    if (errors.length > 0) {
+      setError('password', { type: 'manual', message: 'Invalid password' });
+    } else {
+      clearErrors('password');
+    }
+  };
+
   if (isSuccess) return <SuccessScreen />;
 
   return (
     <FormContainer>
-      <h2>Sign Up</h2>
+      <SignUpTxt>Sign Up</SignUpTxt>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <Input
+        <EmailInput
           {...register('email', { required: 'Email is required' })}
           placeholder="Email"
+          onChange={handleEmailChange}
         />
         {errors.email && <span>{errors.email.message}</span>}
 
-        <Input
-          {...register('password', { required: 'Password is required' })}
-          placeholder="Password"
-          type="password"
-        />
+        <br />
+        {/*<PasswordInput*/}
+        {/*  {...register('password', { required: 'Password is required' })}*/}
+        {/*  placeholder="Password"*/}
+        {/*  type="password"*/}
+        {/*  onChange={handlePasswordChange}*/}
+        {/*/>*/}
+
+        <PasswordContainer>
+          <PasswordInput
+            {...register('password', { required: 'Password is required' })}
+            onChange={handlePasswordChange}
+            type={showPassword ? 'text' : 'password'}
+            placeholder="Create your password"
+            onFocus={() => setIconColor('#007bff')}
+            onBlur={() => setIconColor('#aaa')}
+          />
+          <ShowHidePasswordButton onClick={togglePasswordVisibility} iconColor={iconColor} aria-label="Toggle Password">
+            {showPassword ? <HidePasswordIcon /> : <ShowPasswordIcon />}
+          </ShowHidePasswordButton>
+        </PasswordContainer>
+
+
+        <div>
+          {passwordValidationErrors.map((error, index) => (
+            <span key={index} style={{ color: 'red', display: 'block' }}>
+              {error}
+            </span>
+          ))}
+        </div>
         {errors.password && <span>{errors.password.message}</span>}
 
-        <Button type="submit">Sign Up</Button>
+        <PasswordRulesContainer>
+          {/*TODO extract to function*/}
+          <span>8 characters or more (no spaces)</span>
+          <br />
+          <span>Uppercase and lowercase letters</span>
+          <br />
+          <span>At least one digit</span>
+          <br />
+        </PasswordRulesContainer>
+
+
+        <SignUpButton type="submit">Sign Up</SignUpButton>
       </form>
     </FormContainer>
   );
