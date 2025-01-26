0cf7a5f28cee5e789e061a550520a73a23cacaba
Myroslav Sokolov <alexazimov333@gmail.com>
2025-01-26 23:40:14 +0100

    Add regex

--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -1,8 +1,10 @@
 export const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
 export const passwordRegex = {
   minLength: 8,
   maxLength: 64,
   uppercase: /[A-Z]/,
+  lowercase: /[a-z]/,
   number: /\d/,
   noSpaces: /^\S*$/,
 };
